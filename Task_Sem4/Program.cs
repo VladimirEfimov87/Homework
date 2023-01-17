8cc92696d6b12e7c9ecc9049ebd314cf7b6753d4
Vladimir <rtk2171@gmail.com>
2023-01-17 21:06:23 +0500

    Task_Sem4

---
+++ Task_Sem4/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+//  и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// Console.WriteLine("Введите число A: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число B: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+// double result = Math.Pow(A, B);
+// Console.WriteLine(result);
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+//  82 -> 10
+// //  9012 -> 12
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+// while (num > 0)
+// {
+//     int n = num % 10;
+//     num = num / 10;
+//     sum = sum + n;
+// }
+// Console.WriteLine(sum);
+
+
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 16, , 33 -> [6, 1, 33]
+
+
+// int[] array = new int[8];
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(0, 100);
+//     Console.WriteLine($"{array[i]}");
+// }
+
+
+
+
